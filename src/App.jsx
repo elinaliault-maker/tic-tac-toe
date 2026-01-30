585f8d68dadc36c22dea021c7581bb7a6f4e7615
Elina Liault <elina.liault@gmail.com>
2026-01-30 14:38:45 +0100

    avancement 30/01

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,39 @@ function Square(props) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    // because null is falsy but X and O are truthy so if filled do the return
+    // or if there is already a winner
+    if (squares[i] || calculateWinner(squares)){
+      return;
+    }
+    // if its null than fill it by the correct value
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    
+    if(xIsNext) {
+      nextSquares[i] = "X";
+    }
+    else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+  
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
   }
 
   return (
   <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -39,4 +62,24 @@ export default function Board() {
     </div>
   </>
 );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
