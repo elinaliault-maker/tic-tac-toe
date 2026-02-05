cb092ae40463a3ed4c7613937deb861f821c0ee0
Elina Liault <elina.liault@gmail.com>
2026-02-05 11:40:34 +0100

    Use 2 loops to make the squares rather than hardcoding

--- src/App.jsx
+++ src/App.jsx
@@ -38,26 +38,38 @@ function Board({ xIsNext, squares, onPlay }) {
     status = `Next player: ${xIsNext ? "X" : "O"}`;
   }
 
+  const boardGrid = () => {
+    let rowSquares = [];
+    let boardRows = [];
+    let startNumber = 0;
+    // pour chaque ligne
+    for (let i = 0; i < 3; i++) {
+      // pour chaque square
+      for (let j = 0; j < 3; j++) {
+        let k = startNumber + j;
+        rowSquares = [
+          ...rowSquares, 
+          <Square key={k} value={squares[k]} onSquareClick={() => handleClick(k)} />
+        ];
+      }
+      // add the squares to the row
+      boardRows = [
+        ...boardRows, 
+        <div className="board-row">{rowSquares}</div>
+      ];
+      // reset
+      rowSquares = [];
+      startNumber = startNumber + 3;
+    }
+    return boardRows;
+  }
+
   return (
-  <>
-    <div className="status">{status}</div>
-    <div className="board-row">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-    </div>
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-    </div>
-  </>
-);
+    <>
+      <div className="status">{status}</div>
+      {boardGrid()}
+    </>
+  );
 }
 
 export default function Game() {
@@ -90,7 +102,7 @@ export default function Game() {
     } else {
       tag = <button onClick={() => jumpTo(move)}>{description}</button>;
     }
-    
+
     return (
       <li key={move}>
         {tag}
